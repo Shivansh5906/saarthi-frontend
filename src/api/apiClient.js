8b17e942855eb50a4dc29852ded31e3e7658bf86
shivansh <shivanshchitranshi1009@gmail.com>
2025-11-29 19:16:41 +0530

    your commit message

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://saarthi-bank-backend-production.up.railway.app/api"
+  baseURL: "https://saarthi-bank-backend-production.up.railway.app"
 });
 
 api.interceptors.request.use((config) => {
