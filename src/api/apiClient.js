a63594844dffb22fb4174d49c3a88e262d727ecd
Shivansh5906 <shivanshchitranshi1009@gmail.com>
2026-06-29 15:56:03 +0530

    Use Render backend URL

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://saarthi-bank-backend.onrender.com"
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = token;
